b8f0f0c037e20957e227ddd1d5cf23bb3f06e577
Juanma Arenas <111137831+juanmatII@users.noreply.github.com>
2025-04-27 18:14:47 -0500

    Add files via upload

--- project_newspaper/script.js
+++ project_newspaper/script.js
@@ -1,21 +1,63 @@
-function crearNoticias(titulo = '',descripcion ='', categoria = '', publico = true){
-    /* let noticia = {
+function crearNoticias(
+  titulo = "",
+  descripcion = "",
+  categoria = "",
+ /*  publico = true */
+) {
+  /* let noticia = {
         titulo:titulo,
         descripcion: descripcion,
         categoria : categoria,
         publico : publico,
         Fecha : new Date().toLocaleString()
     } */
-   const crearDivNoticia = document.createElement('div');
-   const crearTituloNoticia = document.createElement('h2');
-   const crearDescripcionNoticia = document.createElement('p');
-   const crearCategoriaNoticia = document.createElement('p');
-   const crearTipoDeAcceso = document.createElement('p');
-   const crearFechaNoticia = document.createElement('p');
+  const muro = document.getElementById("muroNoticias");
+  const crearDivNoticia = document.createElement("div");
+  const crearTituloNoticia = document.createElement("h2");
+  const crearDescripcionNoticia = document.createElement("p");
+  const crearCategoriaNoticia = document.createElement("p");
+  const crearTipoDeAcceso = document.createElement("p");
+  const crearFechaNoticia = document.createElement("p");
+  let fechaHoy = new Date().toLocaleString();
+
+
+
+  crearTituloNoticia.textContent=titulo;
+  crearDescripcionNoticia.textContent=descripcion;
+  crearCategoriaNoticia.textContent=categoria;
+  crearFechaNoticia.textContent=fechaHoy;
+
+  crearDivNoticia.classList.add("noticia");
+
+
+
+
+
+  crearDivNoticia.appendChild(crearTituloNoticia);
+  crearDivNoticia.appendChild(crearDescripcionNoticia);
+  crearDivNoticia.appendChild(crearCategoriaNoticia);
+  crearDivNoticia.appendChild(crearFechaNoticia);
+
+  muro.appendChild(crearDivNoticia);
+}
+
+
+
+function generarNoticia(event) {
+   event.preventDefault();
+  const tituloNoticia = document.getElementById("titulo-creacion-formulario");
+  const contenidoNoticia = document.getElementById("descripcion");
+  const fechaNoticia = document.getElementById("fechaNoticia");
+  const categoriaNoticia = document.getElementById("categoria");
+  const accesoPublico = document.getElementById("accesoPublico");
+
+  let titulo = tituloNoticia.value,
+    contenido = contenidoNoticia.value,
+    categoria = categoriaNoticia.value,
+    publico = accesoPublico.checked;
    
-   crearDivNoticia.classList('noticia');
-
-   
+   console.warn(typeof publico)
 
 
+  crearNoticias(titulo,contenido,categoria)
 }
