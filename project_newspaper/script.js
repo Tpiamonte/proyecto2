ccda8e3491257ae6d5d8ab43cda7ea3db7605b8c
Juanma Arenas <111137831+juanmatII@users.noreply.github.com>
2025-04-27 16:53:28 -0500

    Add files via upload

---
+++ project_newspaper/script.js
@@ -0,0 +1,21 @@
+function crearNoticias(titulo = '',descripcion ='', categoria = '', publico = true){
+    /* let noticia = {
+        titulo:titulo,
+        descripcion: descripcion,
+        categoria : categoria,
+        publico : publico,
+        Fecha : new Date().toLocaleString()
+    } */
+   const crearDivNoticia = document.createElement('div');
+   const crearTituloNoticia = document.createElement('h2');
+   const crearDescripcionNoticia = document.createElement('p');
+   const crearCategoriaNoticia = document.createElement('p');
+   const crearTipoDeAcceso = document.createElement('p');
+   const crearFechaNoticia = document.createElement('p');
+   
+   crearDivNoticia.classList('noticia');
+
+   
+
+
+}
